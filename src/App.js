e5a501676cf458b558a287c7e960e75cede98727
Lagzkun <lagzlight@.com>
2022-06-17 22:37:05 -0400

    primera version con pagination

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Card, Button, Row, Col } from 'react-bootstrap';
+import {API} from '../src/constants/index'
+import ReactPaginate from 'react-paginate';
 
 
 function App() {   
   const [state, setState] = useState([]);
+  const [search, setSearch] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [errorSearch, setErrorSearch] = useState('');
+  const [pages, setPages] = useState(42);
 
+  //pagination
+  let pageChange = (data) => {
+    setCurrentPage(data.selected + 1)
+  }
+
+  const [width, setWidth] = useState(window.innerWidth);
+  const updateDimensions = () => {
+    setWidth(window.innerWidth);
+  };
+
+  const services = `${API}/?page=${currentPage}&name=${search}`;
   useEffect(() => {
     const responseCards = async () => {
-      const request = await fetch('https://rickandmortyapi.com/api/character')
-      const response = await request.json();
-      setState(response.results);
+      const request = await fetch(services)
+      if (request.status === 200) {
+        const response = await request.json();
+   
+        setState(response.results);
+        setPages(response.info.pages)
+        setErrorSearch('')
+      } else if (request.status === 404) {
+        setState([])
+        setErrorSearch('Personaje no encontrado :/')
+      }
     }
-    
     responseCards()
-  })
-  console.log(state);
+  }, [services])
+
+    //render pagination
+    useEffect(() => {
+      window.addEventListener("resize", updateDimensions);
+      return () => window.removeEventListener("resize", updateDimensions);
+    }, []);
+  
+
   return (
     <div className="App">
       <div className='container'> 
+      <input
+          type="text"
+          placeholder="search"
+          onChange={(event) => {
+            setCurrentPage(1);
+            setSearch(event.target.value)
+          }}
+        ></input>
         <Row className="g-4">
       {
         state.map((item) => ( 
@@ -32,14 +71,31 @@ function App() {
                   <p>{item.gender}</p>
                   <p>{item.species}</p>
                 </Card.Text>
-                <Button variant="primary" href={item.origin.url}>{item.origin.name}</Button>
+                <Button variant="primary">{item.origin.name}</Button>
               </Card.Body>
             </Card>
           </Col>
         ))
       }
+      
         </Row>
+        <ReactPaginate
+              className="pagination"
+              nextLabel="Next"
+              forcePage={currentPage === 1 ? 0 : currentPage - 1}
+              previousLabel="Prev"
+              previousClassName="prev-button"
+              nextClassName="prev-button"
+              activeClassName="active"
+              marginPagesDisplayed={width < 576 ? 1 : 2}
+              pageRangeDisplayed={width < 576 ? 1 : 2}
+              pageCount={pages}
+              onPageChange={pageChange}
+              pageClassName="page-item"
+              pageLinkClassName="page-link"
+            />
       </div>
+      <div className="errorMesages">{errorSearch}</div>
     </div>
   );
 }
